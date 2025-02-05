6c317ea632ab72593dcc9d05844ee30990b66824
Mehnaaz Shaik <mehnaazshaik02@gmail.com>
2025-02-05 19:48:26 +0530

    homepage

---
+++ templates/script.js
@@ -0,0 +1,4 @@
+// Add interactivity to the Predict Sentiment button
+document.getElementById('predictButton').addEventListener('click', () => {
+    alert('Predicting sentiment... This feature is under development!');
+  });
